55b68fc312be8cc2fa20cc0fc8197e1d48f9bb6e
Benjamin Demolder <benjamin.demolder@gmail.com>
2017-05-07 15:43:47 +0200

    Add moment and semantic ui

--- app/routes/boardgamenights.js
+++ app/routes/boardgamenights.js
@@ -2,6 +2,40 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return ['Night 1', 'Night 2', 'Night 3'];
+    return [
+      {
+        date: new Date(2017, 5, 20),
+        boardGame: "Blood Rage",
+        organisator: "Benjamin",
+        location: "Merelbeke",
+        weight: 3,
+        filling: {
+          current: 1,
+          max: 4
+        }
+      },
+      {
+        date: new Date(2017, 5, 22),
+        boardGame: "Terra Mystica",
+        organisator: "Benjamin",
+        location: "Gent",
+        weight: 3,
+        filling: {
+          current: 3,
+          max: 4
+        }
+      },
+      {
+        date: new Date(2017, 5, 23),
+        boardGame: "Vast - The Crystal Carverns",
+        organisator: "Benjamin",
+        location: "De Pinte",
+        weight: 3,
+        filling: {
+          current: 4,
+          max: 4
+        }
+      }
+    ];
   }
 });
