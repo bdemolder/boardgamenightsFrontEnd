e1271062c968d5bc64fb255395bbe64d942c79af
Benjamin Demolder <benjamin.demolder@gmail.com>
2017-05-08 23:46:47 +0200

    Add basic modal in component

--- app/components/boardgamenight-list.js
+++ app/components/boardgamenight-list.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    openModal: function(name, id) {
+      $('.ui.' + name + '.modal#profile-' + id)
+      .modal({ dimmerSettings: { opacity: 0.7 }})
+      .modal('show');
+    }
+  }
 });
--- app/routes/boardgamenights.js
+++ app/routes/boardgamenights.js
@@ -6,7 +6,12 @@ export default Ember.Route.extend({
       {
         date: new Date(2017, 5, 20),
         boardGame: "Blood Rage",
-        organisator: "Benjamin",
+        organisator: {
+          id: '99e201ef-3c17-4d8d-a84b-89b26fafe48d',
+          fullName: "Benjamin Demolder",
+          intro: "Kitties! Kitties! Kitties!",
+          age: 29
+        },
         location: "Merelbeke",
         weight: 3,
         filling: {
@@ -17,7 +22,12 @@ export default Ember.Route.extend({
       {
         date: new Date(2017, 5, 22),
         boardGame: "Terra Mystica",
-        organisator: "Benjamin",
+        organisator: {
+          id: '747516a1-3673-4c6d-ba9f-d77743dd7969',
+          fullName: "Fake Name",
+          intro: "Kitties! Kitties! Kitties! 2",
+          age: 30
+        },
         location: "Gent",
         weight: 3,
         filling: {
@@ -28,7 +38,12 @@ export default Ember.Route.extend({
       {
         date: new Date(2017, 5, 23),
         boardGame: "Vast - The Crystal Carverns",
-        organisator: "Benjamin",
+        organisator: {
+          id: 'f36fcaf7-29cf-441a-89bd-e8f1f0ccb73a',
+          fullName: "Faker Name",
+          intro: "Kitties! Kitties! Kitties! 3",
+          age: 31
+        },
         location: "De Pinte",
         weight: 3,
         filling: {
