e3cf2ad40f24e2fd84d5aef0a8ca93de052fbc41
Benjamin Demolder <benjamin.demolder@gmail.com>
2017-05-20 13:01:15 +0200

    Remove obsolete stuff

--- app/components/boardgamenight-list.js
+++ app/components/boardgamenight-list.js
@@ -2,11 +2,8 @@ import Ember from 'ember';
 import $ from 'jquery';
 
 export default Ember.Component.extend({
-  gMap: Ember.inject.service(),
   actions: {
     openModal: function(name, id) {
-      var gMapObject = this.get('gMap');
-
       var elementId = '';
       if (id) {
         elementId = '#' + name + '-' + id;
